dcf5196c11f1aaf563230d22405419283d412ee6
Maggie Flatt <maggieflatt19@gmail.com>
2026-02-05 11:15:21 -0600

    created library component to link library button. Created route for library

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,10 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { Library } from "./library/Library";
 
 export const ApplicationViews = () => {
   return (
     <Routes>
-      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/" element={<Navigate to="/welcome" />} />
+      <Route path="welcome" element={<Welcome />} />
+      <Route path="library" element={<Library />} />
     </Routes>
   );
 };
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,21 +1,18 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 export const Welcome = () => {
+  
   return (
     <main className="bg-charcoal min-h-screen w-full">
       <div className="flex justify-center pt-6">
         <img src={logo} alt="Quest Hall Logo" className="logo-primary" />
       </div>
-      <section className="flex flex-col items-center justify-center gap-8 md:flex-row">
-        <div>
+      <section className="flex flex-col items-center justify-center gap-10 pt-15 md:flex-row">
+        <Link to={"/library"}>
         <button className="btn-secondary">Your Library</button>
-        </div>
-        <div>
-          <div className="text-cream flex justify-center font-serif font-bold">
-            COMING SOON!
-          </div>
+        </Link>
           <button className="btn-secondary">Taverns</button>
-        </div>
       </section>
     </main>
   );
