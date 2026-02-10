3877ac627538bf6e9137b398c38d51efbc73d9ef
Maggie Flatt <maggieflatt19@gmail.com>
2026-02-10 15:53:52 -0600

    Completed gameDetails component. Created plaformServices for gameDetails and changed a game description in database.

--- src/components/games/GameDetails.jsx
+++ src/components/games/GameDetails.jsx
@@ -1,29 +1,64 @@
 import { useEffect, useState } from "react";
-import { getGameById } from "../services/gameServices";
-import { useParams } from "react-router-dom";
+import { deleteGame, getGameById } from "../services/gameServices";
+import { useNavigate, useParams } from "react-router-dom";
+import { getPlatformByUserId } from "../services/platformServices";
 
-export const GameDetails = () => {
+export const GameDetails = ({ user }) => {
   const [game, setGame] = useState({});
+  const [platform, setPlatform] = useState({});
   const { gameId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getGameById(gameId).then(setGame);
   }, [gameId]);
 
+  useEffect(() => {
+    getPlatformByUserId(user?.id, gameId).then((platformArray) => {
+      const platformObj = platformArray[0];
+      setPlatform(platformObj);
+      console.log(platformObj);
+    });
+  }, [user?.id, gameId]);
+
+  const handleDelete = () => {
+    deleteGame(gameId).then(() => {
+      navigate("/games");
+    });
+  };
   return (
-    <div className="bg-gray min-h-screen w-full pt-16">
-      <div className="flex bg-stone border-olive m-10 rounded-lg border border-4 border-solid p-5">
-        <img src={game.imageUrl} alt={game.name} className="h-96 rounded-lg" />
-        <div className="pl-95 text-4xl text-offWhite">{game.name}</div>
-        <section>
-          <div className="flex">{game.description}</div>
-          <div>
-            {/*Platform user owns game on*/}
+    <div className="bg-gray min-h-screen w-full py-16">
+      <div className="bg-stone border-olive m-10 flex flex-col gap-8 rounded-lg border border-4 border-solid p-5 md:flex-row">
+        <img
+          src={game.imageUrl}
+          alt={game.name}
+          className="aspect-[2/3] rounded-lg md:h-80 lg:h-96"
+        />
+        <div className="flex flex-1 flex-col gap-y-10">
+          <div className="text-offWhite text-4xl font-bold">{game.name}</div>
+          <div className="text-charcoal flex flex-col gap-y-2 text-base">
+            <div className="pb-4">
+              <span className="text-mediumGreen font-bold">Description: </span>
+              {game.description}
+            </div>
+            <div className="pb-4">
+              <span className="text-mediumGreen font-bold">Platform: </span>
+              {platform?.platform?.name}
+            </div>
+            <div className="pb-4">
+              <span className="text-mediumGreen font-bold">Game Type: </span>
+              {game.gameType}
+            </div>
           </div>
-          <div>{game.gameType}</div>
-        </section>
-        <button>Edit Game</button>
-        <button>Remove From Library</button>
+        </div>
+        <div className="flex flex-col justify-end gap-2">
+          <button className="btn-four" onClick={() => navigate("/EditGame")}>
+            Edit Game
+          </button>
+          <button className="btn-four" onClick={handleDelete}>
+            Remove From Library
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/services/gameServices.js
+++ src/components/services/gameServices.js
@@ -1,3 +1,9 @@
 export const getGameById = (gameId) => {
 return fetch(`http://localhost:8000/games/${gameId}`).then(res => res.json())
- }
+}
+ 
+export const deleteGame = (gameId) => {
+  return fetch(`http://localhost:8000/games/${gameId}`, {
+    method: "DELETE",
+  })
+}
