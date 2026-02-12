3cadf9b161a0e96518dd80a5c5f695dcf0a18e1f
Maggie Flatt <maggieflatt19@gmail.com>
2026-02-12 09:55:48 -0600

    updated usersGamesServices fetch calls to reflect data change

--- src/components/services/userGamesServices.js
+++ src/components/services/userGamesServices.js
@@ -1,3 +1,13 @@
 export const getUsersGamesByUserId = (userId) => {
-  return fetch(`http://localhost:8000/usersGames/?userId=${userId}&_expand=game`).then(res => res.json())
- }
+  return fetch(`http://localhost:8000/usersGames/?userId=${userId}`).then(res => res.json())
+}
+ 
+export const getUsersGameById = (id) => {
+return fetch(`http://localhost:8000/usersGames/${id}?_expand=platform`).then(res => res.json())
+}
+
+export const deleteUsersGame = (id) => {
+  return fetch(`http://localhost:8000/usersGames/${id}`, {
+    method: "DELETE",
+  })
+}
