92e4f773ef18c23aa7eb1b876949cd66cb928c59
Maggie Flatt <maggieflatt19@gmail.com>
2026-02-12 09:54:17 -0600

    took fetch calls and moved to usersGamesServices to reflect data change.

--- src/components/services/gameServices.js
+++ src/components/services/gameServices.js
@@ -2,8 +2,3 @@ export const getGameById = (gameId) => {
 return fetch(`http://localhost:8000/games/${gameId}`).then(res => res.json())
 }
  
-export const deleteGame = (gameId) => {
-  return fetch(`http://localhost:8000/games/${gameId}`, {
-    method: "DELETE",
-  })
-}
