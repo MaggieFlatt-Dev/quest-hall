e27ddd6de6c36b825dafd9e1e997fc76614d6e99
Maggie Flatt <maggieflatt19@gmail.com>
2026-02-16 15:47:12 -0600

    created tavern details component

--- src/components/taverns/TavernDetails.jsx
+++ src/components/taverns/TavernDetails.jsx
@@ -2,3 +2,9 @@
 // Fetch their games
 // Find shared multiplayer games
 // Display both users and games
+
+export const TavernDetails = () => {
+  return (
+    <div className="py-16">Tavern Details</div>
+  )
+ }
