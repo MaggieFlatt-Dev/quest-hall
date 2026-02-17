df7c080c18aba6e25402699a96683f5d1f7f15cf
Maggie Flatt <maggieflatt19@gmail.com>
2026-02-17 15:02:55 -0600

    created getTaverns and getUsersByTavernId fetch call

--- src/components/services/tavernsServices.js
+++ src/components/services/tavernsServices.js
@@ -1,3 +1,11 @@
-export const getTavernsById = () => {
-  return fetch(`http://localhost:8000/taverns`).then(res=> res.json())
+export const getTaverns = () => {
+  return fetch(`http://localhost:8000/taverns`).then(res => res.json())
+ }
+
+export const getTavernsById = (tavernId) => {
+  return fetch(`http://localhost:8000/taverns/${tavernId}`).then(res => res.json())
+}
+ 
+export const getUsersByTavernId = (tavernId) => {
+  return fetch(`http://localhost:8000/users?tavernId=${tavernId}`).then(res => res.json())
  }
